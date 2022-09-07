7efa09e009d94a76f339042f861277a97cc5aea7
billytrinh <quanghoa.trinh@gmail.com>
2022-09-07 21:10:53 +0700

    js event demo with ajax - weather

--- js/demo3.js
+++ js/demo3.js
@@ -14,3 +14,15 @@ function changeText() {
     h.style.transform = "rotate("+x+"deg)";
     x++;
 }
+function inputSearch(obj) {
+   // var ix = document.getElementById(id);
+    console.log("Searching: "+ obj.value);
+}
+function demoEvent2() {
+    // var ips = document.getElementsByTagName("input");// array
+    // var ips = document.getElementsByClassName("input-text");// array
+    var ips = document.querySelectorAll("input.input-text[type=text]");// array
+    for (var i=0;i<ips.length;i++){
+        ips[i].style.backgroundColor = "deeppink";
+    }
+}
